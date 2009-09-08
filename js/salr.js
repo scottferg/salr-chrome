df7fb4f312d536bd56ce661b9e4ea28acdd7614a
Scott Ferguson <sferguson@quikorder.com>
2009-09-08 14:50:31 -0500

    Add colorpicker

---
+++ js/salr.js
@@ -0,0 +1,178 @@
+// Copyright (c) 2009, Scott Ferguson
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the software nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY SCOTT FERGUSON ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL SCOTT FERGUSON BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+var settings = {};
+
+/**
+ * Event listener for when a user enters their username within
+ * the extension UI.  Currently this only works when you're
+ * viewing forums.somethingawful.com since we don't have any
+ * events that can be fired on a localStorage event that occurs
+ * within the extension.
+ *
+ */
+var port = chrome.extension.connect();
+
+port.onMessage.addListener(function(data) {
+    settings.darkRead = data.darkRead;
+    settings.lightRead = data.lightRead;
+    settings.lightNew = data.lightNewReplies;
+    settings.darkNew = data.darkNewReplies;
+    settings.username = data.username;
+    settings.userQuote = data.userQuote;
+    settings.hideAdvertisements = data.hideAdvertisements;
+    settings.hideFooterLinks = data.hideFooterLinks;
+    settings.hideHeaderLinks = data.hideHeaderLinks;
+
+    // Update the styles now that we have
+    // the settings
+    updateStyling();
+});
+
+// Request the username from the extension UI
+port.postMessage({
+    'message': 'GetPageSettings'
+});
+
+// Since we have to wait to receive the settings from the extension,
+// stash the styling logic in it's own function that we can call
+// once we're ready
+function updateStyling() {
+    // If a post has the user quoted, highlight it a pleasant green
+    jQuery('.bbc-block h4').each(function() {
+        if (jQuery(this).html() == settings.username + ' posted:') {
+            jQuery(this).parent().css("background-color", settings.userQuote);
+        }
+    });
+
+    var newPosts = false;
+    var newPostCount = 0;
+
+    // Iterate over each .thread .seen td element.  This is necessary
+    // so we can track each thread's designated color (read/not read)
+    jQuery('tr.thread.seen').each(function() {
+        // Re-style the "mark unread" link
+        jQuery(this).find('a.x').each(function() {
+            // Set the image styles
+		    jQuery(this).css("background", "none");
+		    jQuery(this).css("background-image", "url('" + chrome.extension.getURL("images/") + "unvisit.png')");
+		    jQuery(this).css("height", "16px");
+		    jQuery(this).css("width", "14px");
+
+    		// Remove the 'X' from the anchor tag
+    		jQuery(this).html('');
+    	});
+
+    	// Re-style the new post count link
+        jQuery(this).find('a.count').each(function() {
+	    	// If we find an a.count, then we have new posts
+    		newPosts = true;
+	    	newPostCount = jQuery(this).html();
+
+		    // Remove the left split border
+    		jQuery(this).css("border-left", "none");
+
+    		// Resize, shift, and add in the background image
+    		jQuery(this).css("width", "7px");
+    		jQuery(this).css("height", "16px");
+    		jQuery(this).css("padding-right", "11px");
+    		jQuery(this).css("background-image", "url('" + chrome.extension.getURL("images/") + "lastpost.png')");
+		
+    		// Remove the count from the element
+    		jQuery(this).html('');
+    	});
+	
+    	// If the thread has new posts, display the green shade,
+    	// otherwise show the blue shade
+    	var darkShade = (newPosts) ? settings.darkNew : settings.darkRead;
+    	var lightShade = (newPosts) ? settings.lightNew : settings.lightRead;
+
+    	// Thread icon, author, view count, and last post
+    	jQuery(this).children('td.icon, td.author, td.views, td.lastpost').each(function() {
+    		jQuery(this).css({ "background-color" : darkShade, 
+    						   "background-image" : "url('" + chrome.extension.getURL("images/") + "gradient.png')",
+    						   "background-repeat" : "repeat-x"
+    						 });
+    	});
+
+    	// Thread title, replies, and rating
+    	jQuery(this).find('td.title, td.replies, td.rating').each(function() {
+    		jQuery(this).css({ "background-color" : lightShade, 
+    						   "background-image" : "url('" + chrome.extension.getURL("images/") + "gradient.png')",
+    						   "background-repeat" : "repeat-x"
+    						 });
+    	});
+
+    	// Display number of new replies for each thread
+    	jQuery(this).find('td.replies').each(function() {
+    		// Add in number of new replies
+    		if (newPostCount != 0) {
+    			var currentHtml = jQuery(this).html();
+    
+    			// Strip HTML tags
+    			newPostCount = parseInt(newPostCount.replace(/(<([^>]+)>)/ig, ""));
+    			// Set the HTML value
+    			jQuery(this).html(currentHtml + "<br /><div style='font-size: 12px;'>(" + newPostCount + ")</div>");
+    		}
+    	});
+    
+    	// Eliminate last-seen styling
+    	jQuery(this).find('.lastseen').each(function() {
+    		jQuery(this).css("background", "none");
+    		jQuery(this).css("border", "none");
+    	});
+
+        // Reset post counts
+    	newPosts = false;
+    	newPostCount = 0;
+    });
+
+    console.log(settings);
+    // Hide header/footer links
+    if (settings.hideHeaderLinks == 'true') {
+        jQuery('div#globalmenu').each(function() {
+            jQuery(this).html('');
+            jQuery(this).css('height', '0px');
+        });
+
+        jQuery('ul#nav_purchase').each(function() {
+            jQuery(this).html('');
+            jQuery(this).css('height', '0px');
+        });
+    }
+
+    // Hide the advertisements
+    if (settings.hideAdvertisements == 'true') {
+        jQuery('div.oma_pal').each(function() {
+            jQuery(this).html('');
+            jQuery(this).css('height', '0px');
+        });
+
+        jQuery('div#ad_banner_user').each(function() {
+            jQuery(this).html('');
+            jQuery(this).css('height', '0px');
+        });
+    }
+}
---
+++ js/settings-handler.js
@@ -0,0 +1,173 @@
+// Copyright (c) 2009, Scott Ferguson
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the software nor the
+//       names of its contributors may be used to endorse or promote products
+//       derived from this software without specific prior written permission.
+// 
+// THIS SOFTWARE IS PROVIDED BY SCOTT FERGUSON ''AS IS'' AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL SCOTT FERGUSON BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+/**
+ * Initialize event callbacks for the page
+ *
+ */
+jQuery(document).ready(function() {
+    // Initialize text entry fields
+    jQuery('.settings-panel > .username-field > input.text-entry').each(function() {
+        // Pre-populate settings field
+        populateValues(jQuery(this));
+
+        // Set focus handler for the entry fields
+        jQuery(this).focus(function() {
+            onInputSelect(jQuery(this));
+        });
+        
+        // Set blur handler for the entry fields
+        jQuery(this).blur(function() {
+            onInputDeselect(jQuery(this));
+        });
+    });
+  
+    // Initialize color entry fields
+    jQuery('.settings-panel > .color-preference > .color-select-input > input.color-select-text').each(function() {
+        populateValues(jQuery(this));
+    });
+
+    // Initialize checkbox fields
+    jQuery('div.display-preference > input').each(function() {
+        populateCheckboxes(jQuery(this));
+    });
+
+    // Set color selectors
+    jQuery('div.color-select-box').each(function() {
+        var backgroundColor = jQuery(this).parent().parent().find('input.color-select-text').val();
+        jQuery(this).css('background-color', backgroundColor);
+    });
+ 
+    // Set click handler for the okay button
+    jQuery('.submit-panel > input#submit').click(function() {
+        onSubmitClicked(jQuery(this));
+    });
+    
+    // Setup menu tabs
+    jQuery('#tabs').tabs();
+});
+
+/**
+ * Event handler for focusing on the input
+ *
+ * @param element - Input element
+ *
+ */
+function onInputSelect(element) {
+    element.css('color', '#000000');
+    element.val('');
+}
+
+/**
+ * Event handler for blurring the input
+ *
+ * @param element - Input element
+ *
+ */
+function onInputDeselect(element) {
+    // If the user didn't enter anything,
+    // reset it to the saved value
+    if (element.val() == '') {
+        var value = localStorage.getItem(element.attr('id'));
+
+        element.val(value);
+    }
+
+    element.css('color', '#999999');
+}
+
+/**
+ * Populates the stored settings value into the element
+ *
+ * @param element - Input element
+ *
+ */
+function populateValues(element) {
+    var value = window.opener.localStorage.getItem(element.attr('id'));
+
+    if (!value) {
+        // If there is no stored setting, use the default
+        // value stored within the DOM
+        element.val(element.attr('default'));
+    } else {
+        // Otherwise, write the stored preference
+        element.val(value);
+    }
+}
+
+/**
+ * Populates any checkboxes with their stored value
+ *
+ * @param element - Input (checkbox) element
+ *
+ */
+function populateCheckboxes(element) {
+    var value = window.opener.localStorage.getItem(element.attr('id'));
+
+    // Make sure we're getting passed a checkbox
+    if (element.attr('type') != 'checkbox')
+        return;
+
+    // If there is a value in localStorage, then set it,
+    // otherwise leave it unchecked
+    if (value == 'true') {
+        element.attr('checked', true);
+    }
+}
+
+/**
+ * Event handler for clicking the submit button 
+ *
+ * @param element - Input element
+ *
+ */
+function onSubmitClicked(element) {
+    var usernameField = jQuery('#username', element.parent().parent());
+    var usernameQuote = jQuery('#user-quote', element.parent().parent());
+    var darkReadField = jQuery('#dark-read', element.parent().parent());
+    var lightReadField = jQuery('#light-read', element.parent().parent());
+    var darkNewRepliesField = jQuery('#dark-new-replies', element.parent().parent());
+    var lightNewRepliesField = jQuery('#light-new-replies', element.parent().parent());
+
+    // Set checkbox values
+    var hideAdvertisements = jQuery('#hide-advertisements').attr('checked');
+    var headerLinks = jQuery('#hide-header-links').attr('checked');
+
+    // Store the preferences locally so that the page can
+    // request it
+    // We use window.opener to assign it to the toolstrip localStorage, since
+    // the toolstrip handles all communication with the page
+    window.opener.localStorage.setItem('username', usernameField.val());
+    window.opener.localStorage.setItem('user-quote', usernameQuote.val());
+    window.opener.localStorage.setItem('dark-read', darkReadField.val());
+    window.opener.localStorage.setItem('light-read', lightReadField.val());
+    window.opener.localStorage.setItem('dark-new-replies', darkNewRepliesField.val());
+    window.opener.localStorage.setItem('light-new-replies', lightNewRepliesField.val());
+    window.opener.localStorage.setItem('hide-advertisements', hideAdvertisements);
+    window.opener.localStorage.setItem('hide-header-links', headerLinks);
+
+    // Close the settings window
+    window.close();
+}
