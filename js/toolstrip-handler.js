556f54628304444bfeb9d2a4643d5309a24232bb
Jono Taberner <jono.taberner@gmail.com>
2009-09-24 10:39:19 +0800

    settings window size adjusted for colorpicker
    Signed-off-by: Scott Ferguson <scottwferg@gmail.com>

--- js/toolstrip-handler.js
+++ js/toolstrip-handler.js
@@ -66,5 +66,5 @@ chrome.pageActions["open_settings"].addListener(function(pageActionId, reply) {
 function onToolbarClick() {
     window.open(chrome.extension.getURL('') + 'settings.html', 
                 'salr-settings', 
-                'location=0,scrollbars=0,toolbar=0,resizable=0,menubar=0,status=0,width=500,height=500');
+                'location=0,scrollbars=0,toolbar=0,resizable=0,menubar=0,status=0,width=510,height=510');
 }
