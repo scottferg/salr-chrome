b94ca0bc44da253554e15cb83f8b32540e06de8d
Scott Ferguson <sferguson@quikorder.com>
2009-09-30 10:17:36 -0500

    Turn on Configure SALR link by default

--- js/toolstrip-handler.js
+++ js/toolstrip-handler.js
@@ -102,7 +102,5 @@ function setupDefaultPreferences() {
     localStorage.setItem('dark-new-replies', '#99cc99');
     localStorage.setItem('light-new-replies', '#ccffcc');
     localStorage.setItem('youtube-highlight', '#ff00ff');
-	//image options
-	localStorage.setItem('dont-replace-link-nws', true);
-	
+    localStorage.setItem('display-configure-salr', 'true')	
 }
