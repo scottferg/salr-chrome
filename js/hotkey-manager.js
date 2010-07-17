715aa6b19f31252960c6fb91cc7f0bc6864a4a49
= <scottwferg@gmail.com>
2010-07-17 13:31:34 -0500

    Work all hotkey featured except edit

--- js/hotkey-manager.js
+++ js/hotkey-manager.js
@@ -35,46 +35,63 @@ function HotKeyManager() {
      R - Quick Reply current thread
     *******************/
     this.bindHotKeys();
+    this.current_post = -1;
+
+    this.thread_post_size = jQuery('div#thread > table.post').size();
 }
 
 HotKeyManager.prototype.bindHotKeys = function() {
     var that = this;
 
     jQuery(document).keypress(function(event) {
-        switch(event.keyCode) {
-            case 110:
-                // Next post
-                break;
-            case 112:
-            case 109:
-                // Previous post
-                break;
-            case 106:
-                // Next page
-                that.nextPage();
-                break;
-            case 107:
-            case 104:
-                // Previous page
-                that.previousPage();
-                break;
-            case 111:
-                // Re-anchor thread
-                break;
-            case 113:
-                // Quick quote current post
-                break;
-            case 101:
-                // Quick edit current post
-                break;
-            case 114:
-                // TODO: Conditionalize on quick reply being enabled
-                quickReply.show();
-                break;
-            case 27:
-                // TODO: Conditionalize on quick reply being enabled
-                quickReply.hide();
-                break;
+        var quick_reply_block = false;
+
+        if (findCurrentPage() == 'showthread.php') {
+            if (quickReply.isExpanded() || quickReply.isVisible()) {
+                quick_reply_block = true;
+            }
+        }
+
+        if (!quick_reply_block) {
+            switch(event.keyCode) {
+                case 110:
+                    // Next post
+                    that.nextPost();
+                    break;
+                case 112:
+                case 109:
+                    // Previous post
+                    that.previousPost();
+                    break;
+                case 106:
+                    // Next page
+                    that.nextPage();
+                    break;
+                case 107:
+                case 104:
+                    // Previous page
+                    that.previousPage();
+                    break;
+                case 111:
+                    // Re-anchor thread
+                    that.anchorThread();
+                    break;
+                case 113:
+                    // Quick quote current post
+                    that.quoteCurrentPost();
+                    break;
+                case 101:
+                    // Quick edit current post
+                    break;
+                case 114:
+                    // TODO: Conditionalize on quick reply being enabled
+                    that.displayQuickReply();
+                    break;
+                case 27:
+                    // TODO: Conditionalize on quick reply being enabled
+                    that.hideQuickReply();
+                    break;
+            }
         }
     });
 };
@@ -104,5 +121,79 @@ HotKeyManager.prototype.previousPage = function() {
             this.currentPage = Number(jQuery('span.curpage').html());
             jumpToPage(this.rootPageType, this.basePageID, this.currentPage - 1);
             break;
+    }
+};
+
+HotKeyManager.prototype.nextPost = function() {
+    if (!findCurrentPage() == 'showthread.php') {
+        return;
+    }
+
+    if (this.current_post < (this.thread_post_size - 1)) {
+        this.current_post++;
+    }
+
+    var post = jQuery('div#thread > table.post');
+    var previous_post = post.eq(this.current_post - 1);
+    var current_post = post.eq(this.current_post);
+    previous_post.css('border', '1px solid #c1c1c1');
+    current_post.css('border', '2px dashed #aaa');
+
+    jQuery(window).scrollTop(current_post.offset().top);
+};
+
+HotKeyManager.prototype.previousPost = function() {
+    if (!findCurrentPage() == 'showthread.php') {
+        return;
+    }
+
+    if (this.current_post > 0) {
+        this.current_post--;
+    }
+
+    var post = jQuery('div#thread > table.post');
+    var previous_post = post.eq(this.current_post + 1);
+    var current_post = post.eq(this.current_post);
+    previous_post.css('border', '1px solid #c1c1c1');
+    current_post.css('border', '2px dashed #aaa');
+
+    jQuery(window).scrollTop(current_post.offset().top);
+};
+
+HotKeyManager.prototype.anchorThread = function() {
+    if (!findCurrentPage() == 'showthread.php') {
+        return;
+    } else if (this.current_post == -1) {
+        return;
+    }
+
+    var post = jQuery('div#thread > table.post').eq(this.current_post);
+
+    jQuery(window).scrollTop(post.offset().top);
+};
+
+HotKeyManager.prototype.quoteCurrentPost = function() {
+    if (this.current_post == -1) {
+        return;
+    }
+
+    var current_post = jQuery('div#thread > table.post').eq(this.current_post);
+    var username = jQuery('tr > td.userinfo > dl > dt.author', current_post).html();
+    // Query for the quote
+    var quote = jQuery('tr > td.postbody', current_post).clone();
+
+    quickReply.appendQuote(username, quote);
+    quickReply.show();
+};
+
+HotKeyManager.prototype.displayQuickReply = function() {
+    if (findCurrentPage() == 'showthread.php') {
+        quickReply.show();
+    }
+};
+
+HotKeyManager.prototype.hideQuickReply = function() {
+    if (findCurrentPage() == 'showthread.php') {
+        quickReply.hide();
     }
 };
