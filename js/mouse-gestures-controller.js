07edd11cfcd894b129313ae2bf801ca3f64b254f
Scott Ferguson <scottwferg@gmail.com>
2010-07-17 10:12:07 -0500

    Add visual feedback for mouse gestures

--- js/mouse-gestures-controller.js
+++ js/mouse-gestures-controller.js
@@ -34,6 +34,9 @@ function MouseGesturesController() {
     this.rootPageType = null;
     this.basePageID = null;
 
+    // Page navigation functions are provided globally
+    // in page-navigator.js
+    //
     // TODO: Catch this on user control panel as well
     if (window.location.href != 'http://forums.somethingawful.com/') {
         this.rootPageType = (findCurrentPage() == 'forumdisplay.php') ? 'forumid' : 'threadid';
@@ -44,7 +47,7 @@ function MouseGesturesController() {
 
     jQuery('div#container').each(function() {
         var removeOverlay = function(x, y) {
-            var handler = that.determineLocation(x, y);
+            var handler = that.findFunction(x, y);
             
             jQuery('div#gesture-overlay').each(function() {
                 jQuery(this).remove();
@@ -54,6 +57,49 @@ function MouseGesturesController() {
                 handler.call(that);
             }
         };
+
+        var drawIndicator = function(event) {
+            canvas = document.getElementById('gesture-indicator');
+
+            var overlay_left = jQuery('div#gesture-overlay').offset().left;
+            var overlay_top = jQuery('div#gesture-overlay').offset().top;
+
+            var x_coord = event.offsetX;
+            var y_coord = event.offsetY;
+
+            // If we are out of bounds of the overlay then we need
+            // to adjust accordingly
+            if (event.pageX < (overlay_left + 77)) {
+                x_coord = 0;
+            }
+            
+            if (event.pageY < (overlay_top + 77)) {
+                y_coord = 0;
+            }
+
+            if (event.pageX > (overlay_left + 153)) {
+                x_coord = 77;
+            }
+
+            if (event.pageY > (overlay_top + 153)) {
+                y_coord = 77;
+            }
+
+            context = canvas.getContext('2d');
+
+            // Clear the rectangle and draw the stroke
+            context.clearRect(0, 0, 77, 77);
+            context.lineWidth = '4';
+
+            context.strokeStyle = "rgba(0, 0, 0, .5)";
+            context.beginPath();
+            context.moveTo(38, 38);
+            context.lineTo(x_coord, y_coord);
+            context.stroke();
+            context.closePath();
+
+            that.updateButtonStyles(event.pageX, event.pageY);
+        }
 
         jQuery(this).rightMouseDown(function(event) {
             jQuery('body').append(that.gesture_overlay_html);
@@ -67,6 +113,8 @@ function MouseGesturesController() {
             });
             
             jQuery('div#gesture-overlay').noContext();
+
+            jQuery('div#gesture-overlay').mousemove(drawIndicator);
         });
 
         jQuery(this).rightMouseUp(function(event) {
@@ -80,11 +128,13 @@ MouseGesturesController.prototype.buildOverlay = function() {
            '    <div id="gesture-top">' +
            '        <img src="' + chrome.extension.getURL("images/") + 'gesturenav-top.png">' +
            '    </div>' +
-           '    <div id="gesture-right">' +
-           '        <img src="' + chrome.extension.getURL("images/") + 'gesturenav-right.png">' +
-           '    </div>' +
            '    <div id="gesture-left">' +
            '        <img src="' + chrome.extension.getURL("images/") + 'gesturenav-left.png">' +
+           '    </div>' +
+           '    <canvas id="gesture-indicator" width="77" height="77">' +
+           '    </canvas>' +
+           '    <div id="gesture-right">' +
+           '        <img src="' + chrome.extension.getURL("images/") + 'gesturenav-right.png">' +
            '    </div>' +
            '    <div id="gesture-bottom">' +
            '        <img src="' + chrome.extension.getURL("images/") + 'gesturenav-bottom.png">' +
@@ -92,6 +142,21 @@ MouseGesturesController.prototype.buildOverlay = function() {
            '</div>';
 
     return html;
+};
+
+MouseGesturesController.prototype.bindCanvasEvent = function() {
+    canvas.addEventListener('mousemove', gestureMouseMove, false);
+
+    var gestureMouseMove = function(event) {
+        var x, y;
+
+        // Get the mouse position relative to the canvas element.
+        if (event.layerX || event.layerX == 0) { // Firefox
+          x = event.layerX;
+          y = event.layerY;
+        }
+
+    }
 };
 
 MouseGesturesController.prototype.setPageSpecificCSS = function() {
@@ -110,6 +175,57 @@ MouseGesturesController.prototype.setPageSpecificCSS = function() {
     }
 };
 
+MouseGesturesController.prototype.updateButtonStyles = function(x_coord, y_coord) {
+    var button = null;
+    var gesture_top = jQuery('div#gesture-top');
+    var gesture_bottom = jQuery('div#gesture-bottom');
+    var gesture_left = jQuery('div#gesture-left');
+    var gesture_right = jQuery('div#gesture-right');
+
+    switch (this.determineLocation(x_coord, y_coord)) {
+        case 'top':
+            button = gesture_top;
+            break;
+        case 'bottom':
+            button = gesture_bottom;
+            break;
+        case 'left':
+            button = gesture_left;
+            break;
+        case 'right':
+            button = gesture_right;
+            break;
+        default:
+            break;
+    }
+    
+    gesture_top.css('opacity', '1.0');
+    gesture_bottom.css('opacity', '1.0');
+    gesture_left.css('opacity', '1.0');
+    gesture_right.css('opacity', '1.0');
+
+    if (button) {
+        button.css('opacity', '0.5');
+    }
+};
+
+MouseGesturesController.prototype.findFunction = function(x_coord, y_coord) {
+    switch (this.determineLocation(x_coord, y_coord)) {
+        case 'top':
+            return this.topAction;
+            break;
+        case 'bottom':
+            return this.bottomAction;
+            break;
+        case 'left':
+            return this.leftAction;
+            break;
+        case 'right':
+            return this.rightAction;
+            break;
+    }
+};
+
 MouseGesturesController.prototype.determineLocation = function(x_coord, y_coord) {
     var top_button = jQuery('div#gesture-top');
     var left_button = jQuery('div#gesture-left');
@@ -120,19 +236,19 @@ MouseGesturesController.prototype.determineLocation = function(x_coord, y_coord)
     // it is also in a valid Y coordinate
     if (x_coord > top_button.offset().left && x_coord < top_button.offset().left + 77) {
         if (y_coord > top_button.offset().top && y_coord < top_button.offset().top + 77) {
-            return this.topAction;
+            return 'top';
         }
     } else if (x_coord > left_button.offset().left && x_coord < left_button.offset().left + 77) {
         if (y_coord > left_button.offset().top && y_coord < left_button.offset().top + 77) {
-            return this.leftAction;
+            return 'left';
         }
     } else if (x_coord > right_button.offset().left && x_coord < right_button.offset().left + 77) {
         if (y_coord > right_button.offset().top && y_coord < right_button.offset().top + 77) {
-            return this.rightAction;
+            return 'right';
         }
     } else if (x_coord > bottom_button.offset().left && x_coord < bottom_button.offset().left + 77) {
         if (y_coord > bottom_button.offset().top && y_coord < bottom_button.offset().top + 77) {
-            return this.bottomAction;
+            return 'bottom';
         }
     }
 };
@@ -157,18 +273,12 @@ MouseGesturesController.prototype.topAction = function() {
 };
 
 MouseGesturesController.prototype.rightAction = function() {
-    // If page is showthread.php, goto forumdisplay.php
-    // If page is forumdisplay.php, goto forums.somethingawful.com
-    // If page is forums.somethingawful.com, do nothing
     if (this.is_enabled(this.rightAction)) {
         jumpToPage(this.rootPageType, this.basePageID, this.currentPage + 1);
     }
 };
 
 MouseGesturesController.prototype.leftAction = function() {
-    // If page is showthread.php, goto forumdisplay.php
-    // If page is forumdisplay.php, goto forums.somethingawful.com
-    // If page is forums.somethingawful.com, do nothing
     if (this.is_enabled(this.leftAction)) {
         jumpToPage(this.rootPageType, this.basePageID, this.currentPage - 1);
     }
