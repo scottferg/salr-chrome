e0561c0ef9967a22729dc0b451d5ecdc6804c4f5
Scott Ferguson <scottwferg@gmail.com>
2010-08-17 22:30:12 +0800

    Highlight forums cancer posts

--- extension/js/salr.js
+++ extension/js/salr.js
@@ -115,6 +115,10 @@ SALR.prototype.pageInit = function() {
                 this.addSearchThreadForm();
             }
 
+            if (this.settings.highlightCancer == 'true') {
+                this.highlightCancerPosts();
+            }
+
             this.renderWhoPostedInThreadLink();
 
             break;
@@ -1184,4 +1188,18 @@ SALR.prototype.addRapSheetToProfile = function() {
     jQuery('a',el).text('Rap Sheet');
     link.parent().append(' ');
     link.parent().append(el);
+}
+
+/**
+ * Highlight forums cancer posts with a light gray and set opacity to
+ * 1.0
+ *
+ */
+SALR.prototype.highlightCancerPosts = function() {
+    jQuery('.cancerous').each(function() {
+        jQuery(this).css({
+            'background-color': '#98afc7',
+            'opacity': '1.0'
+        });
+    });
 }
