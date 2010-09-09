65ee5b3f003422012ca86b51df66ebdae6eef408
Scott Ferguson <scottwferg@gmail.com>
2010-09-09 13:08:52 -0500

    Clean up the Waffle Images API

--- extension/js/waffle-images-uploader.js
+++ extension/js/waffle-images-uploader.js
@@ -23,3 +23,132 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+function WaffleImagesUploader() {
+    this.dashdash = '--';
+    this.crlf = '\r\n';
+    this.boundary = '------multipartformboundary' + (new Date).getTime(),
+
+    this.mode = 'file';
+    this.tg_format = 'xml';
+    this.url = 'http://waffleimages.com/upload';
+
+    this._listeners = {};
+}
+
+WaffleImagesUploader.prototype.bind = function(event, subject) {
+    if (typeof this._listeners[event] == "undefined") {
+        this._listeners[event] = [];
+    }
+
+    this._listeners[event].push(subject);
+};
+
+WaffleImagesUploader.prototype.fireEvent = function(event) {
+    if (typeof event == "string") {
+        event = {
+            type: event
+        };
+    }
+
+    if (!event.target) {
+        event.target = this;
+    }
+
+    if (this._listeners[event.type] instanceof Array) {
+        var listeners = this._listeners[event.type];
+
+        for (var index in listeners) {
+            listeners[index].call(this, event);
+        }
+    }
+};
+
+WaffleImagesUploader.prototype.addParameter = function(title, param) {
+    result = '';
+
+    result += this.dashdash;
+    result += this.boundary;
+    result += this.crlf;
+    result += 'Content-Disposition: form-data; name="' + title + '"';
+    result += this.crlf;
+    result += param;
+    result += this.crlf;
+
+    return result;
+}
+
+WaffleImagesUploader.prototype.getRequest = function(filename, filedata) {
+    var result = '';
+
+    result += this.addParameter('mode', this.mode);
+    result += this.addParameter('mode', this.mode);
+    
+    
+    result += this.dashdash;
+    result += this.boundary;
+    result += this.crlf;
+    result += 'Content-Disposition: form-data; name="file"';
+    result += '; filename="' + filename + '"';
+    result += this.crlf;
+
+    result += 'Content-Type: application/octet-stream';
+    result += this.crlf;
+    result += this.crlf; 
+
+    result += filedata;
+    result += this.crlf;
+
+    result += this.dashdash;
+    result += this.boundary;
+    result += this.crlf;
+    
+    result += this.dashdash;
+    result += this.boundary;
+    result += this.dashdash;
+    result += this.crlf;
+
+    return result;
+};
+
+WaffleImagesUploader.prototype.upload = function(image) {
+    var that = this;
+
+    var xhr = new XMLHttpRequest();
+    var upload = xhr.upload;
+    var request = this.getRequest(image.file.name, image.result);
+
+    upload.index = image.index;
+    upload.file = image.file;
+    /*
+    upload.downloadStartTime = new Date().getTime();
+    upload.currentStart = start_time;
+    */
+    upload.currentProgress = 0;
+    upload.startData = 0;
+
+    console.log('Starting to transfer');
+
+    xhr.open("POST", this.url, true);
+    xhr.setRequestHeader('content-type', 'multipart/form-data; boundary=' + this.boundary);
+    xhr.send(request);  
+
+    xhr.onload = function() { 
+        var result = {};
+
+        switch (xhr.status) {
+            case 500:
+                result = {
+                    type: 'onError'
+                };
+                break;
+            default:
+                result = { 
+                    type: 'onComplete',
+                    response: xhr.responseText
+                };
+                break;
+        }
+
+        that.fireEvent(result);
+    };
+};
