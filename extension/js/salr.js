7dd7e3e2f530924ff53b2e6a85b4d7e5bf07667a
mbellas <mbellas@gmail.com>
2013-01-10 16:32:19 -0500

    fixes

--- extension/js/salr.js
+++ extension/js/salr.js
@@ -2041,35 +2041,32 @@ SALR.prototype.tagPostedThreads = function(result, thread_id) {
 
 
 SALR.prototype.swapRetinaEmotes = function() {
-	var test = jQuery('.postbody img');
-	jQuery('.postbody img').each(function() {
-		
-		var item = $(this);
-  		if (
-			(item.attr('src').indexOf('i.somethingawful.com/forumsystem/emoticons/') > -1 || 
-			item.attr('src').indexOf('http://fi.somethingawful.com/images/smilies/') > -1) && 
-		item.attr('src').indexOf('@2x') == -1 ) {
-  			
-  			var height = item.attr('offsetHeight');
-  			var width = item.attr('offsetWidth');
-  			
-  			//console.log('found ' + item.src + '; ' + width + 'x' + height);
-  			
-  			item.attr('height', height);
-  			item.attr('width', width);
-  			
-			$.getJSON(chrome.extension.getURL('/images/emoticons/emoticons.json'), function(list) {
-				var f = retinaFilename(item);
-				
-				if (list.indexOf(f) > 0) {
-					console.log('swapping in' + f);
-					item.attr('src',chrome.extension.getURL('/images/emoticons/'+f));
-				}
-				
-			});
+	$(function() {
+		$.getJSON(chrome.extension.getURL('/images/emoticons/emoticons.json'), function(list) {
 			
-  		}
-	});
+			jQuery('.postbody img').each(function() {
+		
+			var item = $(this);
+	  		if (
+				(item.attr('src').indexOf('i.somethingawful.com/forumsystem/emoticons/') > -1 || 
+				item.attr('src').indexOf('http://fi.somethingawful.com/images/smilies/') > -1) && 
+				item.attr('src').indexOf('@2x') == -1 ) {
+
+					var f = retinaFilename(item);
+				
+					if (list.indexOf(f) > 0) {
+						console.log('swapping in' + f);
+						var height = item.height();
+						var width = item.width();
+						item.attr('src',chrome.extension.getURL('/images/emoticons/'+f))
+							.width(width)
+							.height(height);
+					}
+				
+				}
+			}); //each
+		}); //getjson
+	});//$
 	
 }
 
